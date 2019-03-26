fa997140185e5c73e1b8f76c77a238236f35b5bd
fernandocto.scz <fernandocto.scz@gmail.com>
2019-03-26 10:33:35 -0400

    Change client static to client model

---
+++ db/index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const mongoose =  require('mongoose')
+
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/sales', { useNewUrlParser: true })
+
+//define client schema 
+const clientSchema = new mongoose.Schema({
+  name: String,
+  lastName: String,
+  emails: Array,
+  company: String,
+  type: String,
+  orders: Array,
+})
+
+const Client = mongoose.model('client', clientSchema)
+
+module.exports = { Client }
--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -1,30 +1,31 @@
+'use strict'
 
-class Client {
-  constructor (id, { name, lastName, company, emails, type, orders }) {
-    console.log(id, { name, lastName, company, emails, type, orders });
-    this.id = id
-    this.name = name
-    this.lastName = lastName
-    this.emails = emails
-    this.company = company
-    this.type = type
-    this.orders = orders
-  }
-}
-
-const clientsDB = {}
+const mongoose = require('mongoose')
+const { Client } = require('../../db')
 
 const resolvers = {
   Query: {
-    getClient: ({ id }) => {
+    getClient: (root, { id }) => {
       return new Client(id, clientsDB[id])
     }
   },
   Mutation: {
-    addClient: ({ input }) => {
-      const id = require('crypto').randomBytes(10).toString('hex')
-      clientsDB[id] = input
-      return new Client(id, input)
+    addClient: (root, { input }) => {
+      const newClient = Client({
+        name: input.name,
+        lastName: input.lastName,
+        emails: input.emails,
+        company: input.company,
+        type: input.type,
+        orders: input.orders,
+      })
+      newClient.id = newClient._id
+      return new Promise((resolve, object) => {
+        newClient.save(error => {
+          if (error) rejects(error)
+          else resolve(newClient)
+        })
+      })
     }
   }
 }
